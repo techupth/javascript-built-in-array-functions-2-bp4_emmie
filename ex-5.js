e322449eee706ef2c5a611dc8e42b50872021781
Emmiiez <emmiiez.m@gmail.com>
2024-07-01 16:30:42 +0700

    Update ex-5.js

--- ex-5.js
+++ ex-5.js
@@ -1,12 +1,16 @@
 let crimeRecordNames = ["Theodora Milenko", "Tinashe Benigno", "Mahalia Priyanka", "Maisy Sudhir"];
 
 function searchCrimeRecord(name) {
-  // Start coding here
+  if (name == crimeRecordNames.includes(name)){
+    return name + " has a crime record!!!"
+  }else {
+    return name + " has no crime record."
+  }
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
 //ในกรณีที่ไม่เคยมีประวัติเกี่ยวกับอาชญากรรม
-console.log(searchCrimeRecord("Dilan Sandra")); //"Dilan Sandra has no crime record"
+console.log(searchCrimeRecord("Dilan Sandra"));
 
 //ในกรณีที่เคยมีประวัติเกี่ยวกับอาชญากรรม
-console.log(searchCrimeRecord("Tinashe Benigno")); //"Tinashe Benigno has crime record !!!"
+console.log(searchCrimeRecord("Tinashe Benigno"));
